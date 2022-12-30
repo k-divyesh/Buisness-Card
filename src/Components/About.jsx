9bb5eb7e04034aa72a86a5c641f25959193b6973
divyesh <kdivyesh.cs@gmail.com>
2022-12-31 00:24:34 +0400

    about and interests section jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,13 @@
 export default function About() {
-    return;
+    return (
+        <div className="about">
+            <h4 className="h4Heading">About</h4>
+            <p>
+                I am a frontend developer with a particular interest in making
+                things simple and automating daily tasks. I try to keep up with
+                security and best practices, and am always looking for new
+                things to learn.
+            </p>
+        </div>
+    );
 }
--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -1,3 +1,12 @@
 export default function Interests() {
-    return;
+    return (
+        <div className="interests">
+            <h4 className="h4Heading">Interests</h4>
+            <p>
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon
+                buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee
+                fanatic.
+            </p>
+        </div>
+    );
 }
