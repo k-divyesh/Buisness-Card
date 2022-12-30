b7d1c7b3ada2910d93a7b9b755bd4196f73d6ba6
divyesh <kdivyesh.cs@gmail.com>
2022-12-30 23:38:21 +0400

    info section

---
+++ src/Components/Info.jsx
@@ -0,0 +1,27 @@
+import userImg from '../assets/Rectangle 90.png';
+import EmailIcon from '../assets/Icon.png';
+import LinkedInIcon from '../assets/Vector.png';
+
+function Info() {
+    return (
+        <div className="info">
+            <img className="userImage" src={userImg} alt="User Image" />
+            <div className="general">
+                <h3 className="name">Laura Smith</h3>
+                <div className="jobTitle">Front End Developer</div>
+                <a className="websiteLink">laurasmith.website</a>
+            </div>
+            <div className="buttons">
+                <button>
+                    <img src={EmailIcon} alt="" />
+                    Email
+                </button>
+                <button>
+                    <img src={LinkedInIcon} alt="" /> LinkedIn
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Info;
