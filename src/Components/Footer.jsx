8f3190d4f4e73a055259d8dba744ac81a8f465d6
divyesh <kdivyesh.cs@gmail.com>
2022-12-31 00:37:27 +0400

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,15 @@
+import twitter from '../assets/TwitterIcon.png';
+import faceBook from '../assets/FacebookIcon.png';
+import instagram from '../assets/InstagramIcon.png';
+import github from '../assets/GitHubIcon.png';
+
 export default function Footer() {
-    return;
+    return (
+        <footer>
+            <img src={twitter} />
+            <img src={faceBook} />
+            <img src={instagram} />
+            <img src={github} />
+        </footer>
+    );
 }
